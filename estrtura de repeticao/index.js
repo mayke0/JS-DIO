3f1122e985075bd0f486e5c55c526709e8441d32
mayke0 <mayke2_0@yahoo.com>
2023-11-03 17:41:36 -0300

    atualizacao

---
+++ estrtura de repeticao/index.js
@@ -0,0 +1,6 @@
+// 1) Crie um programa que dado um número imprima a sua tabuada.
+
+const muttiplicarPor = 9
+for(i=0; i<11; i++){
+    console.log(`Tabuada de ${muttiplicarPor}, ${muttiplicarPor} x ${i} = ${muttiplicarPor * i}  `);
+}
