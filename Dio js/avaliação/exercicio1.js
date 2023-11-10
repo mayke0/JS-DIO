715a260c4c04b22ea846129dd31502592ab3b00f
mayke0 <mayke2_0@yahoo.com>
2023-11-10 16:24:59 -0300

    atualizacao

---
+++ Dio js/avaliação/exercicio1.js
@@ -0,0 +1,33 @@
+// 1) Faça um programa que receba a média de um aluno.
+// Caso a média seja < 5 imprima "Reprovado"
+// Caso a média seja >= 5 e < 7 imprima "Recuperação"
+// Caso a média seja >= 7 imprima "Aprovado"
+
+//     Exemplo:
+//         Entrada:
+//             5.5
+//         Saída:
+//             Recuperação
+// const nota1 = 3
+// const nota2 = 4
+// const nota3 = 7
+// const nota4 = 7
+
+// const media = (nota1+nota2+nota3+nota4)/4
+
+// const notas = [5, 6, 7, 7]
+// let numero = 0
+// for(nota in notas){
+//     numero+=notas[nota]
+// }
+// const media = numero/notas.length
+
+// if(media<5){
+//     console.log('Reprovado');
+// }
+// if(media>= 5 && media <7){
+//     console.log('Recuperação');
+// }
+// if(media>= 7){
+//     console.log('Aprovado');
+// }
---
+++ Dio js/avaliação/exercicio2.js
@@ -0,0 +1,35 @@
+// 2) Faça um programa que receba N (quantidade de números) e seus respectivos valores.
+// Imprima o maior número par e o menor número impar.
+
+//     Exemplo:
+//         Entrada:
+//             5
+//             3
+//             4
+//             1
+//             10
+//             8
+
+//         Saída:
+//             Maior número par: 10
+//             Menor número impar: 1
+
+const numeros = [5, 3, 4, 1, 10, 8];
+
+let maiorNumeroPar = 0;
+let menorNumeroImpar = 0;
+
+for (numero in numeros) {
+  if (numeros[numero] % 2 === 0) {
+    if (numeros[numero] > maiorNumeroPar) {
+      maiorNumeroPar = numeros[numero];
+    }
+  } else {
+    
+      menorNumeroImpar = numeros[numero];
+    }if (numeros[numero] < menorNumeroImpar) {
+       menorNumeroImpar = numeros[numero];
+     }
+}
+console.log(maiorNumeroPar);
+console.log(menorNumeroImpar);
